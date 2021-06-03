87b050735f8d926f0313828839c7864d419954ed
panj <260313538@qq.com>
2021-06-03 15:28:10 +0800

    add: numpy----learning class 01

--- numpy_tyro/numpy01.py
+++ numpy_tyro/numpy01.py
@@ -10,11 +10,30 @@ b = numpy.sin(a)
 # pyplot.plot(a, b)
 # pyplot.show()
 
-# 画出离散点>=0
+
 # a = numpy.array([ True,  True,  True,  True,  True,  True,  True,  True,  True,
 #         True,  True, False, False, False, False, False, False, False,
 #        False, False, False], dtype=bool)
 # mask = b>=0
 
-pyplot.plot(a,b,'b-o',a,numpy.sin(2*a),"r-^")
+# pyplot.plot(a,b,'b-o',a,numpy.sin(2*a),"r-^")
+# 画出离散点
+# pyplot.scatter(a,b)
+# 画出离散点-2
+x= pylab.rand(200)
+y= pylab.rand(200)
+size = pylab.rand(200) * 30
+color = pylab.rand(200)
+# 多张图像figure
+# pyplot.figure(1)
+# pyplot.scatter(x, y, size, color)
+# pyplot.colorbar()
+# pyplot.figure(0)
+# pyplot.plot(a, b)
+# 多幅子图
+pyplot.subplot(2,2,1)
+pyplot.scatter(x, y, size, color)
+pyplot.subplot(2,2,2)
+pyplot.plot(a, b)
+pyplot.grid()
 pyplot.show()
