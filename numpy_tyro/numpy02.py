c15107e790bc9a1a312e37e812a3e02bb0164fe0
panj <pj_uestc@163.com>
2021-06-03 18:04:04 +0800

    add: numpy----learning class

--- numpy_tyro/numpy02.py
+++ numpy_tyro/numpy02.py
@@ -1,5 +1,5 @@
 from numpy import *
-
+from  numpy.random import rand
 lst = [0, 1, 2, 3, 5, 4]
 a = array(lst)
 print(a, type(a))
@@ -12,9 +12,26 @@ print(a.ndim)  # 维度
 a.fill(-3.8)  # 充满元素的值，此时由于类型是int32,所有填充的值就会被整型化
 print(a,a[0])
 
-lst1 = [[0, 1],[ 2, 3], [5, 4]]
-b = array(lst1)
-print(b[0:1],b[-1],b[-3],b[::2],b[1,1])
-print(b.shape)
-od = array([21000, 21180, 21240, 22100, 22400])
-print(od[1:]-od[:-1])
+# lst1 = [[0, 1],[ 2, 3], [5, 4]]
+# b = array(lst1)
+# print(b[0:1],b[-1],b[-3],b[::2],b[1,1])
+# print(b.shape)
+# od = array([21000, 21180, 21240, 22100, 22400])
+# print(od[1:]-od[:-1])
+
+c = array([[ 0, 1, 2, 3, 4, 5],
+           [10,11,12,13,14,15],
+           [20,21,22,23,24,25],
+           [30,31,32,33,34,35],
+           [40,41,42,43,44,45],
+           [50,51,52,53,54,55]])
+# 切片为引用机制，但b=a[2:4]时，改变b的值，就会改变a的值。
+print(c[0,4:6])
+print(c[4:,4:])
+print(c[:,2])
+# 申请分配新的内存，需要使用copy方法b=a[2:4].copy()
+
+aa=rand(10)
+print(aa)
+mask = aa>0.5 # mask 必须为布尔数组
+print(aa[mask])
---
+++ numpy_tyro/numpy03.py
@@ -0,0 +1,4 @@
+from numpy import *
+a=array([1+1j,2,3,4])
+
+print(a.dtype)
