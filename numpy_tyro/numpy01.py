4faf9fda734cf23a59cd715b49f29f6b81a13ba3
panj <260313538@qq.com>
2021-06-03 15:15:18 +0800

    add: numpy----learning class 01

--- numpy_tyro/numpy01.py
+++ numpy_tyro/numpy01.py
@@ -1,10 +1,20 @@
 import numpy
 import math
 from matplotlib import pyplot,pylab
-show = numpy.linspace(0,2*math.pi,21)
-print(show)
-b = numpy.sin(show)
-print(b)
 
-pyplot.plot(show,b)
+a = numpy.linspace(0, 2 * math.pi, 21)
+# print(a)
+b = numpy.sin(a)
+# print(b)
+# 使用numpy画出sin图形
+# pyplot.plot(a, b)
+# pyplot.show()
+
+# 画出离散点>=0
+# a = numpy.array([ True,  True,  True,  True,  True,  True,  True,  True,  True,
+#         True,  True, False, False, False, False, False, False, False,
+#        False, False, False], dtype=bool)
+# mask = b>=0
+
+pyplot.plot(a,b,'b-o',a,numpy.sin(2*a),"r-^")
 pyplot.show()
