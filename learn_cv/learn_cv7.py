cd055055b6206c74b862b7cf7f001ffc4775d7a4
panjwangsu.com <pj_uestc@163.com>
2021-11-26 14:58:33 +0800

    add learn opencv hist

--- learn_cv/learn_cv7.py
+++ learn_cv/learn_cv7.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 def draw_hist(img):
     cv2.imshow("img", img)
-    plt.hist(img.ravel(), 256)
+    plt.hist(img.ravel(), 256)  # 将像素源转换为一位数组
     plt.show()
     cv2.waitKey(0)
     cv2.destroyAllWindows()
