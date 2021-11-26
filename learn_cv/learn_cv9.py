42b1bcbb7b4856632fb7401b7b330d2462657eee
panjwangsu.com <pj_uestc@163.com>
2021-11-26 17:29:42 +0800

    add learn opencv numpy

---
+++ learn_cv/learn_cv9.py
@@ -0,0 +1,23 @@
+# numpy操作图像像素点
+import cv2
+import numpy as np
+
+
+def access_pixels(img):
+    print(img.shape)
+    height = img.shape[0]
+    width = img.shape[1]
+    channels = img.shape[2]
+    print("width : %s, height : %s channels : %s" % (width, height, channels))
+    for row in range(height):
+        for col in range(width):
+            for c in range(channels):
+                pv = img[row, col, c]
+                img[row, col, c] = 255 - pv  # 取反操作
+    cv2.imshow("demo",img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    img = cv2.imread("/Users/panjwangsu.com/Desktop/panj_python/learning_ai/124.jpeg")
+    access_pixels(img)
