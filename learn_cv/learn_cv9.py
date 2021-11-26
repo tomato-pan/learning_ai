aa6a36a8a11c0200a242654716aa4d64ab202a7d
panjwangsu.com <pj_uestc@163.com>
2021-11-26 17:37:58 +0800

    add learn opencv numpy

--- learn_cv/learn_cv9.py
+++ learn_cv/learn_cv9.py
@@ -1,8 +1,15 @@
 # numpy操作图像像素点
-import cv2
+import cv2,time
 import numpy as np
 
+def count_time(func):
+    def wrap(img):
+        t1 = time.time()
+        func(img)
+        print(time.time() - t1)
+    return wrap
 
+@count_time
 def access_pixels(img):
     print(img.shape)
     height = img.shape[0]
@@ -14,10 +21,21 @@ def access_pixels(img):
             for c in range(channels):
                 pv = img[row, col, c]
                 img[row, col, c] = 255 - pv  # 取反操作
-    cv2.imshow("demo",img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.imshow("demo", img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+@count_time
+def cv_inverse(img):
+    dst = cv2.bitwise_not(img)
+    cv2.imshow("dst", dst)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     img = cv2.imread("/Users/panjwangsu.com/Desktop/panj_python/learning_ai/124.jpeg")
-    access_pixels(img)
+    # access_pixels(img)
+    cv_inverse(img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
