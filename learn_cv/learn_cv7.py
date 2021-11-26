4f07790e0282cc03c7859b67d1c99798d77f1160
panjwangsu.com <pj_uestc@163.com>
2021-11-26 15:52:34 +0800

    add learn opencv hist

--- learn_cv/learn_cv7.py
+++ learn_cv/learn_cv7.py
@@ -1,6 +1,7 @@
 # 绘制直方图
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def draw_hist(img):  # plt绘制
@@ -21,7 +22,19 @@ def cv_draw_hist(img):  # opencv 绘制 灰色直接[0]，BGR对应[0],[1],[2]
     plt.show()
 
 
+def cv_mask_hist(img):
+    image =cv2.imread("/Users/panjwangsu.com/Desktop/panj_python/learning_ai/124.jpeg",cv2.IMREAD_GRAYSCALE)
+    # image = cv2.cvtColor(img, cv2.IMREAD_GRAYSCALE) # 通过转化不行
+    mask = np.zeros(image.shape, np.uint8)
+    mask[200:400, 200:400] = 255
+    histMI = cv2.calcHist([image], [0], mask, [256], [0, 255])
+    hist = cv2.calcHist([image], [0], None, [256], [0, 255])
+    plt.plot(hist, color='r')
+    plt.plot(histMI)
+    plt.show()
+
+
 if __name__ == '__main__':
     img = cv2.imread("/Users/panjwangsu.com/Desktop/panj_python/learning_ai/124.jpeg")
     # draw_hist(img)
-    cv_draw_hist(img)
+    cv_mask_hist(img)
