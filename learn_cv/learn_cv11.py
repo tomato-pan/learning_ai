be893b3579b8626f83033396577b45efc0455647
panjwangsu.com <pj_uestc@163.com>
2021-12-02 10:24:49 +0800

    add learn opencv cv2 brightness

--- learn_cv/learn_cv11.py
+++ learn_cv/learn_cv11.py
@@ -12,12 +12,19 @@ def logic_demo(img1, img2):
     cv2.imshow("no", no)
 
 
+def brightness_demo(img, c, b):
+    h, w, ch = img.shape
+    blank = np.zeros([h, w, ch], img.dtype)
+    dst = cv2.addWeighted(img, c, blank, 1 - c, b)
+    cv2.imshow("brightness", dst)
+
+
 if __name__ == '__main__':
     m1 = cv2.imread("/Users/panjwangsu.com/Desktop/panj_python/learning_ai/124.jpeg")
     m2 = cv2.imread("/Users/panjwangsu.com/Desktop/panj_python/learning_ai/123.jpeg")
     img1 = cv2.resize(m1, (400, 400))
     img2 = cv2.resize(m2, (400, 400))
     logic_demo(img1, img2)
-
+    brightness_demo(m1, 1, 0)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
