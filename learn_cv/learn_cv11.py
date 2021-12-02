ca9552337e1c8b592118d1f86ada03f2d3887d45
panjwangsu.com <pj_uestc@163.com>
2021-12-02 18:23:06 +0800

    add learn opencv cv2 ROI

--- learn_cv/learn_cv11.py
+++ learn_cv/learn_cv11.py
@@ -2,13 +2,14 @@
 import cv2 as cv
 import numpy as np
 
-src = cv.imread("/Users/panjwangsu.com/Desktop/panj_python/learning_ai/124.jpeg")
-cv.namedWindow("input image", cv.WINDOW_AUTOSIZE)
-cv.imshow("input image", src)
-face = src[200:350, 120:360]#脸部区域
-gray = cv.cvtColor(face, cv.COLOR_BGR2GRAY)#转灰色
-backface = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)#
-src[200:350, 120:360] = backface
-cv.imshow("new image", src)
-cv.waitKey(0)
-cv.destroyAllWindows()
+def roi_demo():
+    src = cv.imread("/Users/panjwangsu.com/Desktop/panj_python/learning_ai/124.jpeg")
+    cv.namedWindow("input image", cv.WINDOW_AUTOSIZE)
+    cv.imshow("input image", src)
+    face = src[200:350, 120:360]#脸部区域
+    gray = cv.cvtColor(face, cv.COLOR_BGR2GRAY)#转灰色
+    backface = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)#
+    src[200:350, 120:360] = backface
+    cv.imshow("new image", src)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
