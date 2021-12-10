2cddcf7047284c4f5e27fc08ba227a11bae0ca8e
panjwangsu.com <pj_uestc@163.com>
2021-12-10 11:07:22 +0800

    add learn ocr

---
+++ learn_cv/ocr_demo1.py
@@ -0,0 +1,16 @@
+import pytesseract as tess
+import cv2
+
+image = cv2.imread("/Users/panjwangsu.com/Desktop/panj_python/learning_ai/125.png")
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+text = tess.image_to_string(image_rgb, lang="chi_sim")
+print(text)
+h, w, c = image.shape
+boxes = tess.image_to_boxes(image)
+for b in boxes.splitlines():
+    b = b.split(' ')
+    image = cv2.rectangle(image, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
+
+cv2.imshow('text detect', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
